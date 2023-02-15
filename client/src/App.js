9f6930613db7c25401841b9e4077ee84c0132414
Igor Boyko <iboyko@gmail.com>
2023-02-15 14:35:54 -0800

    added react-router

--- client/src/App.js
+++ client/src/App.js
@@ -21,27 +21,7 @@ function App() {
             <a className="author">Igor Boyko</a>
             <time>2023-01-06 16:45</time>
           </p>
-          <p>Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop1" alt="" />
-        </div>
-        <div className="text">
-          <h2>Full-house battery backup comming later this year</h2>
-          <p>Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop1" alt="" />
-        </div>
-        <div className="text">
-          <h2>Full-house battery backup comming later this year</h2>
-          <p>Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
+          <p className="summary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
         </div>
       </div>
 
