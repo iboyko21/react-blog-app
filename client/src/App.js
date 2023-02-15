f1cfd8d1832d390eb37c966ea703e701fc9d8de9
Igor Boyko <iboyko@gmail.com>
2023-02-15 15:08:38 -0800

    set up components and routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,15 @@ import './App.css';
 import Post from './Post';
 import Header from './Header';
 import {Route, Routes} from 'react-router-dom';
+import Layout from './Layout';
 
 function App() {
   return (
     <Routes>
-      <Route index element={
-    <main>
-      <Header />
-      <Post />
-      <Post /> 
-      <Post /> 
-    </main>
-  } />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Post />} />
+        <Route path={'/login'} element={<div>login page</div>} />
+      </Route> 
     </Routes>
     
   );
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
     return(
         <header>
-        <a href="" className="logo">My Blog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
+            <Link to="/" className="logo">MyBlog</Link>
+            <nav>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
+            </nav>
       </header>
     );
 }
