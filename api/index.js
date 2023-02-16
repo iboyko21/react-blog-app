131bb05cd264acefd47cf9c8290a1c407a4dc236
Igor Boyko <iboyko@gmail.com>
2023-02-15 17:04:19 -0800

    before MongoDB

--- api/index.js
+++ api/index.js
@@ -1,1 +1,16 @@
-// this is index.js for the backend
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json()); 
+
+app.get('/test', (req, res) => {
+    res.json('test ok'); // test route
+});
+app.post('/register', (req, res) => {
+    const {username,password} = req.body;
+    res.json({requestData:{username,password}});
+});
+
+app.listen(4000);
