8b0b2b88c99c2371a3c10f8f1f5485d692101773
Igor Boyko <iboyko@gmail.com>
2023-02-16 11:13:36 -0800

    login and register functionality added and works

--- api/index.js
+++ api/index.js
@@ -3,11 +3,13 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcryptjs'); // used to encrypt passwords
+const jwt = require('jsonwebtoken'); 
 const app = express();
 
 const salt = bcrypt.genSaltSync(10); // for encrypting passwords
+const secret ='khagf087gad986gaga6968gasd'; // used for username cookie
 
-app.use(cors());
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json()); // use json parser
 
 mongoose.connect('mongodb+srv://blog:LUiPdJIY5UAGkwSn@cluster0.ihtfozl.mongodb.net/?retryWrites=true&w=majority');
@@ -25,6 +27,20 @@ app.post('/register', async (req, res) => {
     res.json(userDoc);
     } catch(e) {
         res.status(400).json(e);
+    }
+});
+app.post('/login', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+        // logged in
+        jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+            if (err) throw err;
+            res.cookie('token', token).json('ok');
+        });
+    } else {
+        res.status(400).json('wrong credentials');
     }
 });
 
