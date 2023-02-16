a9b1f5113303999cd8c5b8b4bb84764e12984ff6
Igor Boyko <iboyko@gmail.com>
2023-02-16 10:34:10 -0800

    added bcryptjs encryption for passwords

--- api/index.js
+++ api/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
+const bcrypt = require('bcryptjs'); // used to encrypt passwords
 const app = express();
+
+const salt = bcrypt.genSaltSync(10); // for encrypting passwords
 
 app.use(cors());
 app.use(express.json()); // use json parser
@@ -15,7 +18,10 @@ app.get('/test', (req, res) => { // test route
 app.post('/register', async (req, res) => {
     const {username,password} = req.body;
     try {
-    const userDoc = await User.create({username,password});
+    const userDoc = await User.create({
+        username,
+        password:bcrypt.hashSync(password,salt)
+    });
     res.json(userDoc);
     } catch(e) {
         res.status(400).json(e);
