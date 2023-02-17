059adddc8f4a215f0f7d5de260a11b70016b3e42
Igor Boyko <iboyko@gmail.com>
2023-02-17 10:19:46 -0800

    minor fix to css style logout

--- client/src/Header.js
+++ client/src/Header.js
@@ -31,7 +31,7 @@ export default function Header() {
         {username && (
           <>
             <Link to="/create">Create new post</Link>
-            <a onClick={logout}>Logout ({username})</a>
+            <a className="logout" onClick={logout}>Logout ({username})</a>
           </>
         )}
         {!username && (
